ff7c12d2e18a2576f74d01b5199337795b8d73d2
Mathieu <dlacreme@gmail.com>
2018-07-27 15:24:02 +0200

    fix stock & pdt & order display

--- back/app/controllers/product_controller.rb
+++ back/app/controllers/product_controller.rb
@@ -16,31 +16,13 @@ class ProductController < ApplicationController
             },
           ]},
         :product_prices => {
-          
+
         }
       })
   end
 
   def show
-    data Product
-      .where(id: params[:id])
-      .includes([:product_prices, :stocks => [:stock_format, :stock_type]])
-      .where('product_prices.end_date IS null || product_prices.end_date > NOW()').references(:product_prices)
-      .first().as_json(include: {
-        :stocks => {
-          include: [
-            :stock_type,
-            :stock_format => {
-              include: [
-                :stock_unit
-              ]
-            },
-          ]},
-        :product_prices => {
-          
-        }
-      })
-          
+    data load(params[:id])
   end
 
   def create
@@ -61,10 +43,8 @@ class ProductController < ApplicationController
     form.model.update_product_stocks(ps[:stocks])
     form.model.update_price(ps[:product_price])
     process_form form, {:name => ps[:name], :desc => ps[:desc], :product_category_id => ps[:product_category_id]}
-    data Product
-      .where(id: form.model.id)
-      .as_json()
-      .first()
+
+    data load(form.model.id)
   end
 
   def destroy
@@ -78,5 +58,25 @@ private
     params.permit(:name, :desc, :product_category_id, product_price: [:price, :member_price, :start_date], stocks: [:product_stock_id, :stock_id, :quantity])
   end
 
+  def load(stock_id)
+    Product
+    .where(id: stock_id)
+    .includes([:product_prices, :stocks => [:stock_format, :stock_type]])
+    .where('product_prices.end_date IS null || product_prices.end_date > NOW()').references(:product_prices)
+    .first().as_json(include: {
+      :stocks => {
+        include: [
+          :stock_type,
+          :stock_format => {
+            include: [
+              :stock_unit
+            ]
+          },
+        ]},
+      :product_prices => {
+
+      }
+    })
+  end
 
 end
