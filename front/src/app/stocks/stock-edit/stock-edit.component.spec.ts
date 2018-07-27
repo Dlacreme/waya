30f35bf0e196cacf103e78599afabcaeb36ea766
Mathieu Delacroix <mathieu.delacroix.fr@gmail.com>
2018-07-27 17:38:03 +0200

    wip test (#5)

--- front/src/app/stocks/stock-edit/stock-edit.component.spec.ts
+++ front/src/app/stocks/stock-edit/stock-edit.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { StockEditComponent } from './stock-edit.component';
+import { InputComponent } from '../../form/input/input.component';
+import { SelectComponent } from '../../form/select/select.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatFormFieldModule, MatSelectModule, MatDialogModule } from '@angular/material';
+import { StockService } from '../../api/stock.service';
 
 describe('StockEditComponent', () => {
   let component: StockEditComponent;
@@ -8,7 +12,17 @@ describe('StockEditComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ StockEditComponent ]
+      declarations: [
+        StockEditComponent,
+        InputComponent,
+        SelectComponent,
+      ],
+      imports: [
+        MatDialogModule
+      ],
+      providers: [
+        StockService
+      ]
     })
     .compileComponents();
   }));
@@ -16,7 +30,29 @@ describe('StockEditComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(StockEditComponent);
     component = fixture.componentInstance;
+    component.nameOptions = inputOption();
+    component.descOptions = inputOption();
+    component.balanceOptions = inputOption();
+    component.sizeOptions = inputOption();
+    component.formatOptions = selectOption();
+    component.typeOptions = selectOption();
     fixture.detectChanges();
+
+    function selectOption() {
+      return {
+        label: 'Format',
+        placeholder: 'Format',
+        default: 1,
+        items: []
+      }
+    }
+
+    function inputOption() {
+      return {
+        placeholder: "Description",
+        default: 1
+      }
+    }
   });
 
   it('should create', () => {
