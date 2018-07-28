3dec379e0ff643296ee25f1fdc191b384e6ec740
Mathieu <dlacreme@gmail.com>
2018-07-28 11:40:28 +0200

    wip

--- front/src/app/products/product/product.component.ts
+++ front/src/app/products/product/product.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ProductDto } from '../../api/product.service';
 import { Product } from '../../models/product';
+import { MatDialog } from '@angular/material';
+import { ValidationDialogComponent } from '../../form/validation-dialog/validation-dialog.component';
 
 @Component({
   selector: 'app-product',
@@ -12,11 +14,11 @@ export class ProductComponent implements OnInit {
   public data:Product;
   public canEdit = false;
   public isEditable = false;
+  public canSeeCompo = true;
 
   @Input()
   set product(product:ProductDto) {
     this.data = new Product(product);
-    console.log(this.data);
   }
 
   @Input()
@@ -24,9 +26,25 @@ export class ProductComponent implements OnInit {
     this.canEdit = canEdit;
   }
 
-  constructor() { }
+  @Input()
+  set showcompo(showCompo:boolean) {
+    this.canSeeCompo = showCompo;
+  }
+
+  constructor(
+    private dialog:MatDialog,
+  ) { }
 
   ngOnInit() {
+  }
+
+  public showCompo():void {
+    const dialog = this.dialog.open(ValidationDialogComponent, {
+      data: `Do you want to delete ${this.data.name}?`
+    });
+    dialog.afterClosed().subscribe((result) => {
+      console.log('wow');
+    });
   }
 
 }
