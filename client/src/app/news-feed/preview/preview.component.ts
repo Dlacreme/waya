d983018b923cf2f470915549679238a15a56c853
Mathieu <dlacreme@gmail.com>
2018-09-22 15:09:46 +0700

    wip

--- client/src/app/news-feed/preview/preview.component.ts
+++ client/src/app/news-feed/preview/preview.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input } from '@angular/core';
 import { ArticleDto, EventDto } from '../../services/social.service';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-preview',
@@ -10,17 +11,21 @@ export class PreviewComponent {
 
   public articleData:ArticleDto = {} as ArticleDto;
   public eventData:EventDto = undefined;
+  public pictureUrl:string;
 
   constructor() { }
 
   @Input()
   set article(article:ArticleDto) {
     this.articleData = article;
+    this.pictureUrl = `${environment.wayaApi}${this.articleData.picture_url}`
   }
 
   @Input()
   set event(event:EventDto) {
     this.eventData = event;
+    this.articleData = event.article;
+    this.pictureUrl = `${environment.wayaApi}${this.articleData.picture_url}`
   }
 
 }
